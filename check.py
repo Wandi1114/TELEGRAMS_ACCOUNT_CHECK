9f93f2a9de93571a7cebb2ae98feea67d5a1cf8a
Wandi1114 <59136141+Wandi1114@users.noreply.github.com>
2021-12-18 10:37:09 +0800

    Update check.py

--- check.py
+++ check.py
@@ -1,22 +1,36 @@
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+# from threading import Thread
+# import time
+from multiprocessing import Pool
 
+def pre(withN):
+    for i in range (len(withN)):
+        withN[i] = withN[i].strip('\n')
+    
+    return withN
 
 def check(name):
     html = urlopen(f"https://t.me/{name}").read()
     soup = BeautifulSoup(html, "html.parser")
-
+    name = name.strip('\n')
     if soup.find_all("div","tgme_page_title") :
+        # print(name, ": True")
         return True
     else:
+        # print(name, ": False")
         return False
     
+if __name__ == "__main__":
+    # start = time.perf_counter()
+    names = pre(open("names.txt", "r").readlines())
 
-names = open("names.txt", "r")
-# print(check("f23f23"))
-# print(names.readlines()[0])
-for name in names.readlines() :
-    print(name.strip('\n'),":",check(name))
-    # print(name.strip('\n'))
-names.close()
+    with Pool(5) as p:
+        res = p.map(check,names)
+
+    for i in range(len(res)):
+        print(names[i],":",res[i])
+
+    # finish = time.perf_counter()
+    # print("TIME : ", finish-start)
